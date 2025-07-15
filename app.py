0521cac49230a8d157c5860257a2204b66c08cb8
baptprr <baptiste_poirier@yahoo.fr>
2025-07-15 14:45:01 +0200

    working processing + lite refacto

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
 import argparse
 from datetime import datetime
-import json
 import pandas as pd
 
 from azure.storage.blob import BlobServiceClient
-from utils import schema_check, retrieve_csv_data, check_date_format
+from utils import (
+    schema_check,
+    retrieve_csv_data, 
+    check_date_format, 
+    move_file_in_blob,
+    AZURE_CONTAINER,
+    CONNECTION_STRING,
+    SCHEMA
+)
 
-AZURE_CONTAINER = "76byj86oc9kf"
-REF_BLOB = ["clients", "stores","products"]
 
-with open("connection_string.txt") as f:
-    connection_string = f.read()
-
-with open("schema.json") as j:
-    schema = json.load(j)
 
 def main():
-    parser = argparse.ArgumentParser(description='Script qui accepte une liste de dates.')
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         '--dates',
         type=check_date_format,
         nargs='*',
         default=[datetime.now().strftime("%Y-%m-%d")],
-        help='Liste de dates au format AAAA-MM-JJ. Par défaut, utilise la date du jour.'
+        help='Date list (format YYYY-MM-DD) to process. Default to execution date.'
     )
     args = parser.parse_args()
     dates = args.dates
+    print(f"Ingestion will process files from date(s) : {dates} ")
     
-    storage_client = BlobServiceClient.from_connection_string(connection_string)
+    storage_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
     container = storage_client.get_container_client(AZURE_CONTAINER)
 
     # Get referential csv
@@ -35,24 +36,59 @@ def main():
     stores_df = retrieve_csv_data(container, "stores.csv")
     products_df = retrieve_csv_data(container, "products.csv")
 
+    # For each referential df, check format and schema. Move file if schema is wrong
     for ref_blob_name, ref_df in [("clients", clients_df),
                                 ("stores", stores_df),
                                 ("products", products_df)]:
-        if not schema_check(ref_df, schema[ref_blob_name]):
-            # message + déplacer fichier dans erreurs
-            continue
-        # print(ref_df.head())
+        if not schema_check(ref_df, SCHEMA[ref_blob_name]):
+            print(f"{ref_blob_name} file is corrupted. Moving it to 'errors' folder")
+            move_file_in_blob(storage_client, 
+                              ref_blob_name + ".csv", 
+                              "errors/" + ref_blob_name + "_" + datetime.now().strftime("%Y-%m-%d") + ".csv"
+                              )
+            if ref_blob_name != "clients":
+                continue
+            else:
+                print("""Clients file is needed to proceed to transactions ingestion, but file is corrupted.
+                      Ingestion process is terminated here, waiting for clients file to be repaired.""")
+
     
+    # Remove parenthesis from latlng field
     stores_df["latlng_clean"] = stores_df["latlng"].str.replace('(', '').str.replace(')', '')
+    # Extract latitude and longitude from latlng_clean field by splitting at ','
     stores_df[["latitude","longitude"]] = stores_df["latlng_clean"].str.split(',', expand=True)
+    # Casting latitude and longitude to the right type
     stores_df['latitude'] = stores_df['latitude'].astype(float)
     stores_df['longitude'] = stores_df['longitude'].astype(float)
+    # Drop useless columns
     stores_df.drop(columns=["latlng_clean", "latlng"], inplace=True)
-    print(stores_df.head())
+
+    # Get transactions df (current day or specified dates)
+    blob_to_retrieve = []
+    for blob in container.list_blobs("transactions"):
+        if any([d in blob.name for d in dates]) and blob.name.endswith(".csv"):
+            blob_to_retrieve.append(blob.name)
+
+    transactions_df = pd.DataFrame(columns = SCHEMA["transactions"].keys())
+    
+    for blob in blob_to_retrieve:
+        blob_df = retrieve_csv_data(container, blob)
+        if not schema_check(blob_df, SCHEMA["transactions"]):
+            print(f"{blob} file is corrupted. Moving it to 'errors' folder")
+            move_file_in_blob(storage_client,blob,"errors/" + blob)
+            continue
+        transactions_df = pd.concat([transactions_df, blob_df])
+    
+    # concat date, hour, minute to make a datetime field (or timestamp)
+    transactions_df['datetime'] = pd.to_datetime(transactions_df["date"] + ' ' + transactions_df["hour"].astype(str) + ':' + transactions_df["minute"].astype(str), format='%Y-%m-%d %H:%M')
+    # Add account_id from clients_df
+    transactions_df = transactions_df.merge(clients_df[['account_id','id']], how="left", left_on="client_id",right_on="id")
+    transactions_df.drop(columns=["id"])
+    print(transactions_df.head())
 
 
-    # for blob in container.list_blobs("transactions"):
-    #     print(blob.name)
+
+    
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,10 +1,20 @@
 import argparse
 from datetime import datetime
 import io
+import json
 import pandas as pd
-from azure.storage.blob import ContainerClient
+from azure.storage.blob import ContainerClient, BlobServiceClient
 
-def check_date_format(date_string):
+AZURE_CONTAINER = "76byj86oc9kf"
+REF_BLOB = ["clients", "stores","products"]
+
+with open("connection_string.txt") as f:
+    CONNECTION_STRING = f.read()
+
+with open("schema.json") as j:
+    SCHEMA = json.load(j)
+
+def check_date_format(date_string : str):
     try:
         datetime.strptime(date_string, "%Y-%m-%d")
         return date_string
@@ -12,9 +22,11 @@ def check_date_format(date_string):
         raise argparse.ArgumentTypeError("Wrong date format. Expecting YYYY-MM-DD.")
 
 
-def schema_check(dataframe : pd.DataFrame, expected_schema:dict):
+def schema_check(dataframe : pd.DataFrame, expected_schema : dict):
     if set(dataframe.columns) != set(expected_schema.keys()):
         print("Mismatching columns between dataframe and expected schema")
+        # print(set(dataframe.columns))
+        # print(dataframe.head)
         return False
     
     for col, col_type in expected_schema.items():
@@ -25,9 +37,35 @@ def schema_check(dataframe : pd.DataFrame, expected_schema:dict):
         
     return True
 
-def retrieve_csv_data(container: ContainerClient , blob_name, delimiter=';'):
+def retrieve_csv_data(container: ContainerClient , blob_name : str, delimiter : str =';'):
     blob_client = container.get_blob_client(blob_name)
     blob_data = blob_client.download_blob()
     data = blob_data.readall()
     csv_data = pd.read_csv(io.BytesIO(data), delimiter=delimiter)
     return csv_data
+
+def move_file_in_blob(storage_client: BlobServiceClient,
+                      source_blob_name : str,
+                      destination_blob_name : str,
+                      source_container_name : str = AZURE_CONTAINER,
+                      destination_container_name : str = AZURE_CONTAINER,
+                      delete : bool = False):
+
+    # get blob clients
+    source_blob_client = storage_client.get_blob_client(container=source_container_name, blob=source_blob_name)
+    destination_blob_client = storage_client.get_blob_client(container=destination_container_name, blob=destination_blob_name)
+
+    # copy blob from source to destination
+    destination_blob_client.start_copy_from_url(source_blob_client.url)
+
+    # wait for copy to be finished
+    while True:
+        props = destination_blob_client.get_blob_properties()
+        if props.copy.status != 'pending':
+            break
+
+    # If asked, delete the source blob
+    if delete:
+        source_blob_client.delete_blob()
+
+    print(f"File moved from {source_container_name}/{source_blob_name} to {destination_container_name}/{destination_blob_name}")
